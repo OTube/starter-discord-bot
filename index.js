b86c7526f3f7892b15ce248f148a53d03b19eca7
Oleg <69583657+OTube@users.noreply.github.com>
2023-03-28 16:09:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,113 +1,87 @@
-
-// const { clientId, guildId, token, publicKey } = require('./config.json');
-require('dotenv').config()
 const APPLICATION_ID = process.env.APPLICATION_ID 
 const TOKEN = process.env.TOKEN 
 const PUBLIC_KEY = process.env.PUBLIC_KEY || 'not set'
 const GUILD_ID = process.env.GUILD_ID 
 
+const Discord = require('discord.js');
+const Pinger = new Discord.Client();
 
-const axios = require('axios')
-const express = require('express');
-const { InteractionType, InteractionResponseType, verifyKeyMiddleware } = require('discord-interactions');
+const config = {
 
+  "prefix": "unitaz", //Add your prefix here
 
-const app = express();
-// app.use(bodyParser.json());
+  "ownerOnly": true, //If set to true, only bot owner can control Pinger. If set to false, everyone can use Pinger.
 
-const discord_api = axios.create({
-  baseURL: 'https://discord.com/api/',
-  timeout: 3000,
-  headers: {
-	"Access-Control-Allow-Origin": "*",
-	"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-	"Access-Control-Allow-Headers": "Authorization",
-	"Authorization": `Bot ${TOKEN}`
-  }
+  "ownerID": "1012802456066981908", //Id of bot owner for "ownerOnly" function.
+
+  "channelName": "1090253504251179080", //Name of channel, where will be all pings sent.
+
+  "pingInterval": "1000", //Time in ms (miliseconds). 1000 recommended - If 1000ms (1s), bot will send ping every 1000ms (1s).
+
+  "token": "MTA5MDI1MDc4MDM2NTI5OTczMg.GKgcs9.ZD6D7efzpbdYIRUqM3_s2Okq6J8jdEN_yUZKV4" //Bot token from discord app.
+
+}
+
+Pinger.on('ready', async () => {
+    Pinger.user.setActivity('Iam normal bot :kappa:');
+    console.log(`${Pinger.user.username} is online!`);
 });
 
+Pinger.on('message', async message => {
+    let prefix = config.prefix;
+    if (!message.content.startsWith(prefix)) return;
 
-
-
-app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
-  const interaction = req.body;
-
-  if (interaction.type === InteractionType.APPLICATION_COMMAND) {
-    console.log(interaction.data.name)
-    if(interaction.data.name == 'yo'){
-      return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          content: `Unitaz polon govna, ${interaction.member.user.username}!`,
-        },
-      });
+    async function init() {
+        console.log(1)
+        await sleep(1000)
+        console.log(2)
     }
 
-    if(interaction.data.name == 'dm'){
-      // https://discord.com/developers/docs/resources/user#create-dm
-      let c = (await discord_api.post(`/users/@me/channels`,{
-        recipient_id: interaction.member.user.id
-      })).data
-      try{
-        // https://discord.com/developers/docs/resources/channel#create-message
-        let res = await discord_api.post(`/channels/${c.id}/messages`,{
-          content:'Yo! I got your slash command. I am not able to respond to DMs just slash commands.',
+    function sleep(ms) {
+        return new Promise(resolve => {
+            setTimeout(resolve, ms)
         })
-        console.log(res.data)
-      }catch(e){
-        console.log(e)
-      }
-
-      return res.send({
-        // https://discord.com/developers/docs/interactions/receiving-and-responding#responding-to-an-interaction
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data:{
-          content:'👍'
-        }
-      });
     }
-  }
 
+    if (config.ownerOnly == true && message.author.id !== config.ownerID) {
+        return;
+    } else {
+
+        if (message.content.startsWith(prefix + "ping")) {
+            let user = message.mentions.users.first();
+            if (!user) return message.channel.send(`**${message.author.username}**, you must mention someone!`);
+
+            message.channel.send(`**${message.author.username}**, ping on **${user.tag}** started!`);
+            Pinger.user.setActivity(`Pinging: ${user.tag}`);
+            console.log(`Started pinging on: ${user.tag}!`)
+
+
+            interval = setInterval(async () => {
+
+                let pingChannel = Pinger.channels.find(c => c.name === `${config.channelName}`);
+                if (!pingChannel) {
+                    await message.guild.createChannel(`${config.channelName}`).then(c => {
+                        console.log(`Channel not found. Created new one.\nName: ${c.name}\nID: ${c.id}`);
+                        c.send(`<@${user.id}>`);
+                    });
+                } else {
+                    pingChannel.send(`<@${user.id}>`);
+                }
+
+
+            }, config.pingInterval);
+        };
+
+        if (message.content.startsWith(prefix + "stop")) {
+            await message.channel.send(`**${message.author.username}**, stopping pinging!`);
+            await clearInterval(interval);
+            await sleep(5000);
+            await message.channel.send(`**${message.author.username}**, stopped pinging successfully!`);
+            await Pinger.user.setActivity("Iam normal bot :kappa:")
+            console.log(`Stopped pinging by: ${message.author.tag}`);
+
+        }
+    }
 });
 
-
-
-app.get('/register_commands', async (req,res) =>{
-  let slash_commands = [
-    {
-      "name": "yo",
-      "description": "replies with Yo!",
-      "options": []
-    },
-    {
-      "name": "dm",
-      "description": "sends user a DM",
-      "options": []
-    }
-  ]
-  try
-  {
-    // api docs - https://discord.com/developers/docs/interactions/application-commands#create-global-application-command
-    let discord_response = await discord_api.put(
-      `/applications/${APPLICATION_ID}/guilds/${GUILD_ID}/commands`,
-      slash_commands
-    )
-    console.log(discord_response.data)
-    return res.send('commands have been registered')
-  }catch(e){
-    console.error(e.code)
-    console.error(e.response?.data)
-    return res.send(`${e.code} error from discord`)
-  }
-})
-
-
-app.get('/', async (req,res) =>{
-  return res.send('Follow documentation ')
-})
-
-
-app.listen(8999, () => {
-
-})
-
+Pinger.login(config.token);
